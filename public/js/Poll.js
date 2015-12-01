36e4a568b94479c20c9e0ef2bd245d574a9899d3
Matt <mattmanhardt@gmail.com>
2015-12-01 11:52:53 -0700

    submit button opens modal

--- public/js/Poll.js
+++ public/js/Poll.js
@@ -47,8 +47,24 @@ var Quiz = React.createClass({
                         <div id="question" ><h1>How much do you agree with the following statments?</h1></div>
                             	{statementsList}
 
-                                <button type="button" className="btn btn-primary" onClick={this.handleClick}>SUBMIT</button>
+                                <button type="button" className="btn btn-primary" onClick={this.handleClick} data-toggle="modal" data-target="#myModal">SUBMIT</button>
                         </div>
+	                <div id="myModal" className="modal fade" role="dialog">
+	                     <div className="modal-dialog">
+	                          <div className="modal-content">
+	                               <div className="modal-header">
+                                           <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                           <h4 className="modal-title">Modal Header</h4>
+	                               </div>
+	                               <div className="modal-body">
+                                           <Demographics/>
+	                               </div>
+	                               <div className="modal-footer">
+                                           <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+	                               </div>
+	                         </div>
+	                     </div>
+	                </div>
                         <div className="col-md-6">
                         </div>
                     </div>
